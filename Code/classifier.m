3c3c726692e49eb9c7055e7ab978f5ea85638c5b
Michael C. Frank <mcfrank@stanford.edu>
2014-01-30 22:28:18 -0800

    classifier revisions

--- Code/classifier.m
+++ Code/classifier.m
@@ -1,8 +1,10 @@
 close all;
 clear all; 
 
-file = '/Users/Gui/ja_detect/Code/PartialMatrices/';
+file = 'PartialMatrices/';
 kids = {'0801','0805','0807','0811','0815','1604','1612','1622','1635'};
+
+folds = 10; % folds for cross validation
 
 %% 10-fold NB classification 
 
@@ -16,30 +18,27 @@ for k = 1:length(kids)
 
     % average parameter weights
     descriptives(:,:,k) = [mean(d(~logical(gt_frame),3:6)~=0); ...
-   mean(d(logical(gt_frame),3:6)~=0)];
+    mean(d(logical(gt_frame),3:6)~=0)];
     
     % classifier with 10-fold
-    indices = crossvalind('Kfold', gt_frame, 10);
+    indices = crossvalind('Kfold', gt_frame, folds);
 
-    for i = 1:10
-        test = (indices==i); train = ~test;    
+    for f = 1:folds
+        test = (indices==f); train = ~test;    
         nb = NaiveBayes.fit (d(train,:),gt_frame(train));
-        preds = nb.predict(d(test,:));
-        %cMat1 = confusionmat(gt_frame(test),preds) ;
-        f = gt_frame(test);
-        fold_avg(k,i) = mean(preds == gt_frame(test));
-        
+        preds = nb.predict(d(test,:));       
+        [p_val(k,f) r_val(k,f) f_val(k,f) a_val(k,f)] = calcPRF(preds,gt_frame(test));
+               
         % extract parameters
-        for p = 1:9
-            params(k,i,p,:) = nb.Params{p};
+        for p = 1:size(nb.Params,2)
+            params(k,f,p,:) = nb.Params{p};
         end
     end
+    
+    
 end
 
 %% now look at results
-fold_avg
-mean(fold_avg,2)
-
 m_params = squeeze(mean(params,2));
 
 figure (1);
@@ -54,7 +53,7 @@ for i = 1:9
     
 end
 %saveas(gcf, 'test.png')
-export_fig /Users/Gui/ja_detect/CogSci14/Image/weights.pdf
+% export_fig /Users/Gui/ja_detect/CogSci14/Image/weights.pdf
 
 figure (2);
 for i = 1:9
@@ -71,6 +70,7 @@ xlabel('Sp = Speed;  L = Length;  Sze = Size   ')
 
 %%
 figure(3)
+clf
 m_descriptives = mean(descriptives,3);
 colormap([1 0 0; 0 0 1]);
 subplot(1,2,1)
@@ -82,19 +82,24 @@ title('Descriptive statistics')
 
 subplot(1,2,2)
 bar(squeeze(mean(m_params)))
-legend('no JA','JA','Location','NorthWest')
+% legend('no JA','JA','Location','NorthWest')
 set(gca,'XTickLabel',{'Chunk Length','Chunk Speed',...
     'Face Speed','Face Size','Object Speed','Object Size'});
 ylabel('Parameter weight')
 title('Mean Classifer Parameter Weights')
 xlabel('Feature')
-% 
-% a=get(gca,'XTickLabel');
-% set(gca,'XTickLabel',[]);
-% b=get(gca,'XTick');
-% c=get(gca,'YTick');
-% rot=90;
-% text(b,repmat(c(1)+2*(c(2)-c(1)),length(b),1),a,'HorizontalAlignment','right','rotation',rot,'FontSize', 13, 'FontWeight', 'normal');
-% 
-% set(gca,'ButtonDownFcn','selectmoveresize');
-export_fig /Users/Gui/ja_detect/CogSci14/Image/individual.pdf
+axis([0 7 0 0.025])
+rotateXLabels(gca,90)
+
+%% output tex tables 
+mp_val = mean(p_val,2);
+mr_val = mean(r_val,2);
+ma_val = mean(a_val,2);
+
+for k = 1:length(kids)
+  fprintf('%s & %0.2f & %0.2f & %2.2f & & & \\\\\n',...
+    kids{k},mp_val(k),mr_val(k),ma_val(k))
+end
+
+[mean(mean(p_val,2)) mean(mean(r_val,2)) mean(mean(a_val,2))]
+  
